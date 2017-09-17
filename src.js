45de218a1adf06a4685673fb7be42c5664b1241f
Ali Karbassi <ali@karbassi.com>
2017-09-16 19:02:03 -0500

    Added loading from remote file via `?src=`

--- src.js
+++ src.js
@@ -1,145 +1,161 @@
-const hiddenImage = document.createElement('img');
-const canvas = document.getElementById('canvas');
-const context = canvas.getContext('2d');
-const hexResult = document.querySelectorAll('.hex')[0];
-const rgbResult = document.querySelectorAll('.rgb')[0];
-const colorResult = document.querySelectorAll('.color')[0];
-const xResult = document.querySelectorAll('.x')[0];
-const yResult = document.querySelectorAll('.y')[0];
+const $canvas = document.getElementById('canvas');
+const context = $canvas.getContext('2d');
+const $hiddenImage = document.createElement('img');
+const $hex = document.querySelector('.hex');
+const $rgb = document.querySelector('.rgb');
+const $color = document.querySelector('.color');
+const $x = document.querySelector('.x');
+const $y = document.querySelector('.y');
+const $error = document.querySelector('.error');
 let pos = {
-	x: 0,
-	y: 0
+  x: 0,
+  y: 0
 };
 
 let isLocked = false;
 
 function rgbToHex(r, g, b) {
-	return '#' + ((r << 16) | (g << 8) | b).toString(16);
+  return '#' + ((r << 16) | (g << 8) | b).toString(16);
 }
 
 function makeCursor(color) {
-	const cursor = document.createElement('canvas');
-	const ctx = cursor.getContext('2d');
+  const $cursor = document.createElement('canvas');
+  const ctx = $cursor.getContext('2d');
 
-	cursor.width = 41;
-	cursor.height = 41;
+  $cursor.width = 41;
+  $cursor.height = 41;
 
-	// Crosshair
-	ctx.beginPath();
-	ctx.moveTo(0, 6);
-	ctx.lineTo(12, 6);
-	ctx.moveTo(6, 0);
-	ctx.lineTo(6, 12);
-	ctx.lineWidth = 1;
-	ctx.lineCap = 'butt';
-	ctx.shadowColor = '#FFF';
-	ctx.shadowBlur = 2;
-	ctx.strokeStyle = '#000';
-	ctx.stroke();
+  // Crosshair
+  ctx.beginPath();
+  ctx.moveTo(0, 6);
+  ctx.lineTo(12, 6);
+  ctx.moveTo(6, 0);
+  ctx.lineTo(6, 12);
+  ctx.lineWidth = 1;
+  ctx.lineCap = 'butt';
+  ctx.shadowColor = '#FFF';
+  ctx.shadowBlur = 2;
+  ctx.strokeStyle = '#000';
+  ctx.stroke();
 
-	// Color circle
-	ctx.beginPath();
-	ctx.arc(25, 25, 14, 0, 2 * Math.PI, false);
-	ctx.lineWidth = 2;
-	ctx.strokeStyle = '#000';
-	ctx.stroke();
-	ctx.fillStyle = color;
-	ctx.fill();
+  // Color circle
+  ctx.beginPath();
+  ctx.arc(25, 25, 14, 0, 2 * Math.PI, false);
+  ctx.lineWidth = 2;
+  ctx.strokeStyle = '#000';
+  ctx.stroke();
+  ctx.fillStyle = color;
+  ctx.fill();
 
-	canvas.style.cursor = 'crosshair';
-	canvas.style.cursor = 'url(' + cursor.toDataURL() + ') 6 6, crosshair';
+  $canvas.style.cursor = 'crosshair';
+  $canvas.style.cursor = 'url(' + $cursor.toDataURL() + ') 6 6, crosshair';
 }
 
-canvas.addEventListener('mousemove', function (event) {
-	if (isLocked) {
-		return;
-	}
+function loadImage(file) {
+  if (typeof (file) === 'string' && file.indexOf('http') === 0) {
+    fetch(file)
+      .then(response => response.blob())
+      .then(blob => {
+        document.body.className = 'active';
+        $hiddenImage.src = URL.createObjectURL(blob);
+      })
+      .catch(() => {
+        $error.textContent = 'This file can\'t be loaded. Try another URL.';
+      });
 
-	pos = {
-		x: event.offsetX,
-		y: event.offsetY
-	};
+    return;
+  }
 
-	if (pos.x > canvas.width || pos.y > canvas.height || pos.x < 1 || pos.y < 1) {
-		return;
-	}
+  if (typeof FileReader !== 'undefined' && file.type.indexOf('image') !== -1) {
+    const reader = new FileReader();
 
-	// x -= 4;
-	// y -= 4;
+    reader.onload = event => {
+      $hiddenImage.src = event.target.result;
+    };
 
-	const ctx = this.getContext('2d');
-	const p = ctx.getImageData(pos.x, pos.y, 1, 1).data;
+    reader.readAsDataURL(file);
+  }
+}
 
-	const hex = rgbToHex(p[0], p[1], p[2]);
-	const rgb = 'rgb(' + p[0] + ',' + p[1] + ',' + p[2] + ')';
-
-	hexResult.textContent = hex.toUpperCase();
-	rgbResult.textContent = rgb;
-	xResult.textContent = pos.x;
-	yResult.textContent = pos.y;
-
-	colorResult.style.backgroundColor = hex;
-
-	makeCursor(hex);
-});
-
-hiddenImage.addEventListener('load', () => {
-	canvas.width = hiddenImage.width;
-	canvas.height = hiddenImage.height;
-	context.clearRect(0, 0, canvas.width, canvas.height);
-	context.drawImage(hiddenImage, 0, 0);
+$hiddenImage.addEventListener('load', () => {
+  $canvas.width = $hiddenImage.width;
+  $canvas.height = $hiddenImage.height;
+  context.clearRect(0, 0, $canvas.width, $canvas.height);
+  context.drawImage($hiddenImage, 0, 0);
 }, false);
 
+$canvas.addEventListener('mousemove', function (event) {
+  if (isLocked) {
+    return;
+  }
 
-canvas.addEventListener('click', event => {
-	if (!isLocked) {
-		console.log(pos.x, pos.y);
-	}
-	isLocked = !isLocked;
-	event.preventDefault();
+  pos = {
+    x: event.offsetX,
+    y: event.offsetY
+  };
+
+  if (pos.x > $canvas.width || pos.y > $canvas.height || pos.x < 1 || pos.y < 1) {
+    return;
+  }
+
+  const ctx = this.getContext('2d');
+  const p = ctx.getImageData(pos.x, pos.y, 1, 1).data;
+
+  const hex = rgbToHex(p[0], p[1], p[2]);
+  const rgb = 'rgb(' + p[0] + ',' + p[1] + ',' + p[2] + ')';
+
+  $hex.textContent = hex.toUpperCase();
+  $rgb.textContent = rgb;
+  $x.textContent = pos.x;
+  $y.textContent = pos.y;
+
+  $color.style.backgroundColor = hex;
+
+  makeCursor(hex);
+});
+
+$canvas.addEventListener('click', event => {
+  if (!isLocked) {
+    console.log(pos.x, pos.y);
+  }
+  isLocked = !isLocked;
+  event.preventDefault();
 });
 
 document.addEventListener('drop', event => {
-	document.body.className = 'active';
+  document.body.className = 'active';
 
-	const files = event.dataTransfer.files;
+  const files = event.dataTransfer.files;
 
-	if (files.length > 0) {
-		const file = files[0];
-		if (typeof FileReader !== 'undefined' && file.type.indexOf('image') !== -1) {
+  if (files.length > 0) {
+    loadImage(files[0]);
+  }
 
-			const reader = new FileReader();
-
-			reader.onload = event => {
-				hiddenImage.src = event.target.result;
-			};
-
-			reader.readAsDataURL(file);
-		}
-	}
-
-	event.preventDefault();
+  event.preventDefault();
 });
 
 document.addEventListener('dragover', event => {
-	document.body.className = 'hover';
-	event.preventDefault();
+  document.body.className = 'hover';
+  event.preventDefault();
 });
 
 document.addEventListener('dragenter', event => {
-	document.body.className = 'hover';
-	event.preventDefault();
+  document.body.className = 'hover';
+  event.preventDefault();
 });
 
-
 document.addEventListener('dragleave', event => {
-	document.body.className = '';
-	event.preventDefault();
+  document.body.className = '';
+  event.preventDefault();
 });
 
 document.addEventListener('dragend', event => {
-	document.body.className = '';
-	event.preventDefault();
+  document.body.className = '';
+  event.preventDefault();
 });
 
-
+const searchParams = new URLSearchParams(window.location.search); // ?src=123
+const src = searchParams.get('src');
+if (src) {
+  loadImage(src);
+}
